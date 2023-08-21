1229395a5975703afa5056d744b4160ab3b3d7d1
RKDeriya <141213684+RKDeriya@users.noreply.github.com>
2023-08-21 18:25:35 +0530

    Trigger Update

--- JioMart.beforeInsertOrderDetailsCheckCartOrderStatus.sql
+++ JioMart.beforeInsertOrderDetailsCheckCartOrderStatus.sql
@@ -7,16 +7,26 @@ GO
 -- Create date: <2023-08-20>
 -- Description:	<before insert into orderdetails check the cartorderstatus is Not completed or completed>
 -- =============================================
-CREATE TRIGGER JioMart.beforeInsertOrderDetailsCheckCartOrderStatus 
-   ON JioMart.OrderDetails
-   Before insert
-AS 
+CREATE TRIGGER jiomart.beforeInsertIntoOrderDetailsCheckCartOrderStatusInCartDetails
+ON JioMart.OrderDetails
+INSTEAD OF INSERT
+AS
 BEGIN
-	-- SET NOCOUNT ON added to prevent extra result sets from
-	-- interfering with SELECT statements.
-	SET NOCOUNT ON;
-
-    -- Insert statements for trigger here
-
-END
-GO
+    -- Check if CartOrderStatus is 'Completed' in the CartDetails table
+    IF EXISTS (
+        SELECT 1
+        FROM inserted i
+        INNER JOIN JioMart.CartDetails cd ON i.CartDetailsId = cd.CartDetailsId
+        WHERE cd.CartOrderStatus = 'Completed'
+    )
+    BEGIN
+        THROW 50000, 'Cannot insert data into OrderDetails table when CartOrderStatus is Completed.', 1;
+    END
+    ELSE
+    BEGIN
+        -- Insert the data into OrderDetails table
+        INSERT INTO JioMart.OrderDetails (CartDetailsId,OrderPaymentMode,OrderTotalPrice,OrderDeliveryStatus,OrderDate,Createdon,Updatedon)
+        SELECT i.CartDetailsId,i.OrderPaymentMode,i.OrderTotalPrice,i.OrderDeliveryStatus,i.OrderDate,GETDATE(),GETDATE()
+        FROM inserted i;
+    END
+END;
